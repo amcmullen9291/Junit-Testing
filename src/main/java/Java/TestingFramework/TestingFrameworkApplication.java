8015f5c11ac1760f40b73e8928f301ccc485b8b9
amcmullen9291 <amcmullen9291@gmail.com>
2022-02-22 15:01:35 -0500

    init commit

---
+++ src/main/java/Java/TestingFramework/TestingFrameworkApplication.java
@@ -0,0 +1,13 @@
+package Java.TestingFramework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingFrameworkApplication.class, args);
+	}
+
+}
