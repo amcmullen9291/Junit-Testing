57af46c1498825647c254ea94a2aaa159f15afcf
amcmullen9291 <amcmullen9291@gmail.com>
2022-03-02 13:46:34 -0500

    notes

--- src/test/java/Java/TestingFramework/GraderTest.java
+++ src/test/java/Java/TestingFramework/GraderTest.java
@@ -1,9 +1,18 @@
 package Java.TestingFramework;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GraderTest {
-
+    @Test
+    void fiftyPercentGrade(){
+        var graderTest = new Grader();
+        assertEquals('F', graderTest.determineLetterGrade(50));
+    }
 }
 
 //remember- one test per scenario!
+//if you run the tests with coverage, go to the Class your testing.
+//green lines means that line was tested
+//red means it was not
