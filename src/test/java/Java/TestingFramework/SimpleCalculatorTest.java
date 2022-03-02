907d4f77a23657bfa8ede15ec497bf4b51219ea2
amcmullen9291 <amcmullen9291@gmail.com>
2022-03-02 09:19:12 -0500

    run multiple tests per unit

--- src/test/java/Java/TestingFramework/SimpleCalculatorTest.java
+++ src/test/java/Java/TestingFramework/SimpleCalculatorTest.java
@@ -8,6 +8,16 @@ class SimpleCalculatorTest {
     void addTwoNumbersTest(){
         SimpleCalculator calculator = new SimpleCalculator();
         assertEquals(7,calculator.addTwoNumbers(2,5));
+        //notes- don't depend on only one test to assert functionality
+        assertTrue(calculator.addTwoNumbers(2,2) == 4);
+        //because 2*2 equals 4 as well but that will not work for every case since we are adding
+
+    }
+
+    @Test
+    void addTwoNumbersTest2(){
+        SimpleCalculator calculator = new SimpleCalculator();
+        assertEquals(10,calculator.addTwoNumbers(3,7));
     }
 
 }
